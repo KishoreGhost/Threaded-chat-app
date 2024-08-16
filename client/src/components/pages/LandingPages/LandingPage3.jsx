1338559222c227a7861b2ed2186dba0c471c98a4
Kishore <ki225ku@gmail.com>
2024-08-16 14:26:04 +0530

    made it somewhat responsive and added scroll animation

--- client/src/components/pages/LandingPages/LandingPage3.jsx
+++ client/src/components/pages/LandingPages/LandingPage3.jsx
@@ -1,32 +1,47 @@
 import React from "react";
-import LandPageImg3 from "../../../assets/LP_image_i3.png"
-
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import LandPageImg3 from "../../../assets/LP_image_i3.png";
 
 const LandingPage3 = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   return (
-    <>
-      <div className="flex flex-col lg:flex-row h-auto px-4 justify-around overflow-hidden">
-        <div className="w-full lg:w-[50%] p-8 mt-10 lg:mt-0 flex flex-col justify-center">
-          <div className="font-sans text">
-            <p className="font-semibold text-5xl leading-14">
-              Start connecting directly <br />
-              inside conversations <br />
-            </p>
-            <p className="py-4 font-normal leading-7">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore totam architecto <br/>exercitationem enim ex, doloribus cum ut laudantium laboriosam sequi expedita <br/> consequatur debitis alias cupiditate, doloremque perferendis dolore?<br/>
-            </p>
-          </div>
-          <div className="cursor-pointer flex items-center justify-center bg-[#FB8E0B] border-[#FB8E0B] border-2 border-solid rounded-md w-1/3 p-2 mt-2">
-            <button className="text-white font-normal">
-              Start Chatting now
-            </button>
-          </div>
+    <div ref={ref} className="flex flex-col lg:flex-row h-auto px-4 justify-around overflow-hidden">
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        animate={inView ? { opacity: 1, x: 0 } : {}}
+        transition={{ duration: 0.8 }}
+        className="w-full lg:w-[50%] p-8 mt-10 lg:mt-0 flex flex-col justify-center"
+      >
+        <div className="font-sans text">
+          <p className="font-semibold text-5xl leading-14">
+            Start connecting directly <br />
+            inside conversations <br />
+          </p>
+          <p className="py-4 font-normal leading-7">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+            totam architecto exercitationem enim ex, doloribus cum ut laudantium
+            laboriosam sequi expedita consequatur debitis alias cupiditate,
+            doloremque perferendis dolore?
+          </p>
         </div>
-        <div className="flex justify-center items-center lg:w-[45%] lg:h-[90%]">
-          <img className="h-[45%] lg:h-[100%]" src={LandPageImg3} alt="img" />
+        <div className="cursor-pointer flex items-center justify-center bg-[#FB8E0B] border-[#FB8E0B] border-2 border-solid rounded-md w-1/3 p-2 mt-2">
+          <button className="text-white font-normal">Start Chatting now</button>
         </div>
-      </div>
-    </>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 50 }}
+        animate={inView ? { opacity: 1, x: 0 } : {}}
+        transition={{ duration: 0.8 }}
+        className="flex justify-center items-center lg:w-[45%] lg:h-[90%]"
+      >
+        <img className="h-[45%] lg:h-[100%]" src={LandPageImg3} alt="img" />
+      </motion.div>
+    </div>
   );
 };
 
--- client/src/components/pages/LandingPages/LandingPage4.jsx
+++ client/src/components/pages/LandingPages/LandingPage4.jsx
@@ -1,37 +1,58 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import LandPageImg4 from "../../../assets/LP_image_i4.png";
 
 const LandingPage4 = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   return (
-    <>
-      <div className="flex items-center justify-between p-20 bg-[#f8f8fa]">
-        <div className="flex items-center">
-          <img src={LandPageImg4} alt="" />
+    <div ref={ref} className="flex items-center justify-between p-20 bg-[#f8f8fa]">
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        animate={inView ? { opacity: 1, x: 0 } : {}}
+        transition={{ duration: 0.8 }}
+        className="flex items-center"
+      >
+        <img className="h-[50%] w-[60rem]" src={LandPageImg4} alt="Landing Page Illustration" />
+      </motion.div>
+      
+      <motion.div
+        initial={{ opacity: 0, x: 50 }}
+        animate={inView ? { opacity: 1, x: 0 } : {}}
+        transition={{ duration: 0.8 }}
+        className="flex flex-col items-start justify-between p-10 min-h-[60vh]"
+      >
+        <div className="flex flex-col flex-wrap items-start justify-around px-4 min-h-[10rem]">
+          <h2 className="font-bold text-4xl">Get into</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            suscipit in quas, nobis dignissimos hic unde earum quaerat vitae
+            ducimus iure fugiat! Aut sequi fugiat dignissimos minima ad delectus
+            eaque.
+          </p>
         </div>
-        <div className="flex flex-col items-start justify-between p-10 min-h-[60vh]">
-          <div className="flex flex-col flex-wrap items-start justify-around px-4 mxy-10 min-h-[10rem]">
-            <h2 className="font-bold text-4xl">Get into</h2>
+        
+        <div className="flex flex-wrap items-start justify-between px-4 p-5 w-[80%]">
+          <div className="flex items-center justify-around w-[12rem]">
+            <h1 className="font-medium text-4xl">4.3k+</h1>
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis suscipit in <br />
-              quas, nobis dignissimos hic unde earum quaerat vitae ducimus iure
-              fugiat! Aut <br />
-              sequi fugiat dignissimos minima ad delectus eaque.
+              Website's <br /> Powering
             </p>
           </div>
-          <div className="flex flex-wrap items-start justify-between px-4 mxy-10 p-5 w-[80%]">
-            <div className="flex items-center justify-around w-[12rem]">
-              <h1 className="font-medium text-4xl">4.3k+</h1>
-              <p>Website's <br/> Powering</p>
-            </div>
-            <div className="flex items-center justify-around w-[12rem]">
-              <h1 className="font-medium text-4xl">7M+</h1>
-              <p>Chats in <br/>Last 2024</p>
-            </div>
+          
+          <div className="flex items-center justify-around w-[12rem]">
+            <h1 className="font-medium text-4xl">7M+</h1>
+            <p>
+              Chats in <br /> Last 2024
+            </p>
           </div>
         </div>
-      </div>
-    </>
+      </motion.div>
+    </div>
   );
 };
 
