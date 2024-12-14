186bf11327600ab5d4b0fd125871f94d8d780be4
akhilk49 <akhilkkurian49@gmail.com>
2024-12-14 16:01:28 +0530

    Store and retrieve chat history from MongoDB

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  room: { type: String, required: true },
+  sender: { type: String, required: true },
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
 });
--- backend/socket.js
+++ backend/socket.js
@@ -1,29 +1,41 @@
-const { Server } = require('socket.io');
+const { Server } = require("socket.io");
 
-const handleWebRTCSignaling = require('./controllers/webrtcController');
+const handleWebRTCSignaling = require("./controllers/webrtcController");
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   handleWebRTCSignaling(socket);
 });
-
 
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: '*',
-      methods: ['GET', 'POST'],
+      origin: "*",
+      methods: ["GET", "POST"],
     },
   });
-  
+  const {
+    saveMessage,
+    getChatHistory,
+  } = require("./controllers/chatController");
 
-  io.on('connection', (socket) => {
+  socket.on("sendMessage", async (data) => {
+    await saveMessage(data.room, data.sender, data.content);
+    io.to(data.room).emit("receiveMessage", data);
+  });
+
+  socket.on("joinRoom", async (room) => {
+    const history = await getChatHistory(room);
+    socket.emit("chatHistory", history);
+  });
+
+  io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
 
-    socket.on('sendMessage', (data) => {
-      io.emit('receiveMessage', data);
+    socket.on("sendMessage", (data) => {
+      io.emit("receiveMessage", data);
     });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       console.log(`User disconnected: ${socket.id}`);
     });
   });
