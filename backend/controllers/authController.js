43580b2f49b7551dfbe9038f24b10b9ad2648179
akhilk49 <akhilkkurian49@gmail.com>
2024-12-14 12:13:04 +0530

    Add JWT-based authentication

---
+++ backend/controllers/authController.js
@@ -0,0 +1,12 @@
+const User = require('../models/User');
+const { generateToken } = require('../utils/jwtUtils');
+
+const registerUser = async (req, res) => {
+  // Registration logic here
+};
+
+const loginUser = async (req, res) => {
+  // Login logic here
+};
+
+module.exports = { registerUser, loginUser };
