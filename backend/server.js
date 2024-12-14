b02a1e5c97d19408f3f673c65c4d391ffaf576e4
akhilk49 <akhilkkurian49@gmail.com>
2024-12-14 12:01:16 +0530

    Add Socket.IO for real-time communication

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const http = require('http');
+const initializeSocket = require('./socket');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+const server = http.createServer(app);
+initializeSocket(server);
 
 app.get('/', (req, res) => {
   res.send('Chat App Backend Initialized!');
@@ -14,4 +18,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server with Socket.IO is running on port ${PORT}`);
 });
---
+++ backend/socket.js
@@ -0,0 +1,24 @@
+const { Server } = require('socket.io');
+
+const initializeSocket = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on('sendMessage', (data) => {
+      io.emit('receiveMessage', data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`User disconnected: ${socket.id}`);
+    });
+  });
+};
+
+module.exports = initializeSocket;
