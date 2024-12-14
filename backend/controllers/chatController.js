e60cc09fb4184ececa1612a4eea05aec8fdcd9d1
akhilk49 <akhilkkurian49@gmail.com>
2024-12-14 17:20:17 +0530

    Implement end-to-end encryption for chat messages

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,12 +1,29 @@
-const Message = require('../models/Message');
+const { encrypt, decrypt } = require('../middlewares/encryption');
 
-const saveMessage = async (room, sender, content) => {
-  const message = new Message({ room, sender, content });
-  await message.save();
+// Encrypt message before saving
+const saveMessage = async (req, res) => {
+  const { message, sender, receiver } = req.body;
+  const encryptedMessage = encrypt(message);
+
+  // Mock saving to DB
+  const savedMessage = { sender, receiver, message: encryptedMessage };
+  console.log('Message saved:', savedMessage);
+
+  res.status(201).json({ message: 'Message saved securely', data: savedMessage });
 };
 
-const getChatHistory = async (room) => {
-  return await Message.find({ room }).sort({ timestamp: 1 });
+// Decrypt message when retrieving
+const getMessages = async (req, res) => {
+  const encryptedMessages = [
+    { sender: 'User1', message: 'b1f2c3d4...' }, // Example encrypted messages
+  ];
+
+  const decryptedMessages = encryptedMessages.map((msg) => ({
+    ...msg,
+    message: decrypt(msg.message),
+  }));
+
+  res.status(200).json({ messages: decryptedMessages });
 };
 
-module.exports = { saveMessage, getChatHistory };
+module.exports = { saveMessage, getMessages };
